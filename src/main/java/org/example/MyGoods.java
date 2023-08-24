f1666981e1aec8575e040d7e7501f41a1c6ee71b
Singawe Jack <chenming@mail.ynu.edu.cn>
2023-08-24 16:33:54 +0000

    小改

--- src/main/java/org/example/MyGoods.java
+++ src/main/java/org/example/MyGoods.java
@@ -1,10 +1,13 @@
 package org.example;
 import java.util.Map;
 import java.util.Scanner;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 public class MyGoods {
     private Map<String, MyGoods> goodsMap; // 用Map来存储商品对象，key为name，value为节点
     private Scanner scanner = null;
-    private int id;
+    private String id;
     private String name;
     private String manufacturer;
     private double Price;
@@ -13,7 +16,7 @@ public class MyGoods {
         
     };
 
-    public MyGoods(int id, String name, String manufacturer, double price, int quantity) {
+    public MyGoods(String id, String name, String manufacturer, double price, int quantity) {
         this.id = id;
         this.name = name;
         this.manufacturer = manufacturer;
@@ -21,11 +24,11 @@ public class MyGoods {
         this.quantity = quantity;
     }
 
-    public int getId() {
+    public String getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(String id) {
         this.id = id;
     }
 
@@ -61,8 +64,23 @@ public class MyGoods {
         this.quantity = quantity;
     }
 
-    public void addGoods(MyGoods good){
-        
+    public void addGoods(){
+        System.out.println("请输入goodsID: ");
+        String goodsID=this.scanner.nextLine();
+        System.out.println("请输入goodsname: ");
+        String goodsname=this.scanner.nextLine();
+        System.out.println("请输入manufacturer: ");
+        String manufacturer1=this.scanner.nextLine();
+        System.out.println("请输入price: ");
+        float price=this.scanner.nextFloat();
+        String delete = this.scanner.nextLine();//铲屎
+        System.out.println("请输入quantity:"+delete);
+        int quantity1=this.scanner.nextInt();
+        String delete1 = this.scanner.nextLine();//铲屎
+
+        MyGoods good=new MyGoods(goodsID, goodsname, manufacturer1, price, quantity1);
+        goodsMap.put(good.getName(), good);
+        System.out.println("添加完成！"+delete1);
     }
     public void deltGoods(){
 
@@ -76,5 +94,26 @@ public class MyGoods {
     public void listGoods(){
 
     }
+    public static void writeGoodsToFile(MyGoods goods) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("Goods.txt"))) {
+          // 写入第一行的标题
+        //   writer.write("Id\tname\tmanufacturer\tprice\tquantity");
+        //   writer.newLine();
     
+          // 写入第二行的内容
+          writer.write(goods.getId() + "\t" +
+                       goods.getName() + "\t" +
+                       goods.getManufacturer() + "\t" +
+                       goods.getPrice() + "\t" +
+                       goods.getQuantity());
+          writer.newLine();
+    
+          System.out.println("Goods data written to file successfully.");
+    
+        } catch (IOException e) {
+          System.out.println("An error occurred while writing goods data to file.");
+          e.printStackTrace();
+        }
+      }
+
 }
