0aae55b530611f28d759868e99efcb93a6a0e11b
Singawe Jack <chenming@mail.ynu.edu.cn>
2023-08-26 13:32:37 +0000

    修改

--- src/main/java/org/example/MyAdminstrator.java
+++ src/main/java/org/example/MyAdminstrator.java
@@ -1,18 +1,14 @@
 package org.example;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class MyAdminstrator implements MyAction {
     private static final String ACTION_NAME = "ad";
     private Scanner scanner = null;
-    private Scanner s=new Scanner(System.in);
+   
     
 
     public MyAdminstrator() {
-        List<Myfunction> myList=new ArrayList<Myfunction>();
-        Mygoods goods=new Mygoods(scanner);
-        myList.add(goods);
+        
     }
 
 
@@ -30,14 +26,15 @@ public class MyAdminstrator implements MyAction {
     public void run(String[] args) {
         System.out.print("********欢迎进入管理员菜单********\n");
         System.out.println("请输入管理员的用户ID和密码!");
-        String ID = "";
-        String psw = "";
-        ID=s.nextLine();
-        psw=s.nextLine();
-        System.out.println(ID+"shi1e");
-        System.out.println(psw);
+        // String ID = "";
+        // String psw = "";
+        // ID=s.nextLine();
+        // psw=s.nextLine();
+        // System.out.println(ID+"shi1e");
+        // System.out.println(psw);
         
-        String userInput = "";
+         String userInput = "";
+         String userInput1="";
 
         while(true){
             System.out.println("请输入你的指令,q退出,p密码管理,k客户管理,g商品管理");
@@ -47,17 +44,36 @@ public class MyAdminstrator implements MyAction {
                 break;
             }
             if (userInput.equals("g")){
-                Myfunction A;
-                A=new Mygoods();
-                A.run(null);
+                System.out.println("你现在在**商品管理**三级子菜单下");
+                MyGoods good=new MyGoods(scanner);
+                while(true){
+                    System.out.print("请选择: 1.增加商品 2.删除商品 3.修改商品 4.查询商品 5.列出所有商品 q.退出");
+                    userInput1=this.scanner.nextLine();
+                    if(userInput1.equals("1")){
+                        good.addGoods();
+                    }
+                    else if(userInput1.equals("2")){
+                        good.deltGoods();
+                    }
+                    else if(userInput1.equals("3")){
+                        good.alterGoods();
+                    }
+                    else if(userInput1.equals("4")){
+                        good.readFile();
+                        good.searchGoods();
+                    }
+                    else if(userInput1.equals("5")){
+                        good.listGoods();
+                    }
+                    else if(userInput1.equals("q")){
+                        break;
+                    }
+
+                }
+                good.writeGoodsToFile();
             }
 
-            
-
         }
-
-
-
 
     }
     
--- src/main/java/org/example/MyGoods.java
+++ src/main/java/org/example/MyGoods.java
@@ -1,19 +1,22 @@
 package org.example;
-import java.util.Map;
+import java.util.LinkedHashMap;
+//import java.util.Map;
 import java.util.Scanner;
 import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 public class MyGoods {
-    private Map<String, MyGoods> goodsMap; // 用Map来存储商品对象，key为name，value为节点
+    private static LinkedHashMap<String, MyGoods> goodsMap =new LinkedHashMap<String, MyGoods>();; // 用Map来存储商品对象，key为name，value为节点
     private Scanner scanner = null;
     private String id;
     private String name;
     private String manufacturer;
     private double Price;
     private int quantity;
-    public MyGoods() {
-        
+    public MyGoods(Scanner scanner) {
+        this.scanner=scanner;
     };
 
     public MyGoods(String id, String name, String manufacturer, double price, int quantity) {
@@ -66,9 +69,10 @@ public class MyGoods {
 
     public void addGoods(){
         System.out.println("请输入goodsID: ");
-        String goodsID=this.scanner.nextLine();
+        //String goodsID=scanner.nextLine();
+        String goodsID="123";
         System.out.println("请输入goodsname: ");
-        String goodsname=this.scanner.nextLine();
+        String goodsname=scanner.nextLine();
         System.out.println("请输入manufacturer: ");
         String manufacturer1=this.scanner.nextLine();
         System.out.println("请输入price: ");
@@ -83,31 +87,124 @@ public class MyGoods {
         System.out.println("添加完成！"+delete1);
     }
     public void deltGoods(){
-
+        System.out.println("请输入goodsName: ");
+        String goodsName=this.scanner.nextLine();
+        MyGoods removegoods = goodsMap.remove(goodsName);
+        if (removegoods == null) {
+            System.out.println("删除失败，商品不存在");
+        } else {
+            System.out.println("Have deleted " + removegoods.getName()+" "+removegoods.getName());
+        }
     }
     public void alterGoods(){
+        System.out.println("请输入goodsName: ");
+        String goodsName=this.scanner.nextLine();
+        if(!goodsMap.containsKey(goodsName)){
+            System.out.println("根据商品名找不到该商品");
+        }
+        else{
+            while (true) {
+                System.out.println("What do you want to alter,0 GoodsID,1 Goodsname,2 manufacturer,3 price,4 quantity,q exit");
+                String num = scanner.nextLine();
+                if(num.equals("q")) break;
+    
+                switch (num) {
+                    case "0":
+                        System.out.println("Please enter the GoodsID");
+                        String ID = scanner.nextLine();
+                        goodsMap.get(goodsName).setId(ID);
+                        goodsMap.put(goodsName, goodsMap.get(goodsName));
+                        break;
+                    case "1":
+                        System.out.println("Please enter the Goodsname");
+                        String GoodName=scanner.nextLine();
+                        goodsMap.get(goodsName).setName(GoodName);
+                        goodsMap.put(GoodName, goodsMap.get(GoodName));
+                        break;
+                    case "2":
+                        System.out.println("Please enter the manufacturer");
+                        String Manu = scanner.nextLine();
+                        goodsMap.get(goodsName).setManufacturer(Manu);
+                        goodsMap.put(goodsName, goodsMap.get(goodsName));
+                        break;
+                    case "3":
+                        System.out.println("Please enter the price");
+                        float price=scanner.nextFloat();
+                        String shit=scanner.nextLine();
+                        goodsMap.get(goodsName).setPrice(price);
+                        goodsMap.put(goodsName, goodsMap.get(goodsName));
+                        break;
+                    case "4":
+                        System.out.println("Please enter the quantity");
+                        int quantity =scanner.nextInt();
+                        String shit1=scanner.nextLine();
+                        goodsMap.get(goodsName).setQuantity(quantity);
+                        goodsMap.put(goodsName, goodsMap.get(goodsName));
+                        break;
 
+                    default:
+                        System.out.println("你看看你，这都能输错");
+                        break;
+                }
+                
+                
+            }
+            System.out.println("Alter success!");
+        }
     }
     public void searchGoods(){
+        System.out.println("请输入goodsname: ");
+        String goodsname=this.scanner.nextLine();
+        if(goodsMap.containsKey(goodsname)){
+            System.out.println("有该商品的信息");
+            System.out.println("the GoodsID: "+goodsMap.get(goodsname).getId());
+            System.out.println("the manufacturer: "+goodsMap.get(goodsname).getManufacturer());
+            System.out.println("the price: "+goodsMap.get(goodsname).getPrice());
+            System.out.println("the quantity: "+goodsMap.get(goodsname).getQuantity());
+        }
+        else{
+            System.out.println("商店没有该商品！");
+        }
+        
 
     }
     public void listGoods(){
+        if(goodsMap.keySet().isEmpty()){
+            System.out.println("没有商品!");
+        }
+        else{
+            System.out.println("即将列出所有商品信息!");
+        }
+        for (String goodsname : goodsMap.keySet()) {
+            // 获取键
+            //System.out.println("Key: " + key);
+            // 获取对应的值
+            System.out.println("the GoodsID: "+goodsMap.get(goodsname).getId());
+            System.out.println("the Goodsname"+goodsMap.get(goodsname).getName());
+            System.out.println("the manufacturer: "+goodsMap.get(goodsname).getManufacturer());
+            System.out.println("the price: "+goodsMap.get(goodsname).getPrice());
+            System.out.println("the quantity: "+goodsMap.get(goodsname).getQuantity());
+            System.out.println("------------------------------------");
+        }
+        
 
     }
-    public static void writeGoodsToFile(MyGoods goods) {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter("Goods.txt"))) {
+    public void writeGoodsToFile() {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("Goods.txt",false))) {
           // 写入第一行的标题
-        //   writer.write("Id\tname\tmanufacturer\tprice\tquantity");
-        //   writer.newLine();
-    
-          // 写入第二行的内容
-          writer.write(goods.getId() + "\t" +
-                       goods.getName() + "\t" +
-                       goods.getManufacturer() + "\t" +
-                       goods.getPrice() + "\t" +
-                       goods.getQuantity());
+           writer.write("Id\tname\tmanufacturer\tprice\tquantity");
+           writer.newLine();
+        for (String goodsname : goodsMap.keySet()) {
+            // 获取键
+            //System.out.println("Key: " + key);
+            // 获取对应的值
+            writer.write(goodsMap.get(goodsname).getId() + "\t" +
+                    goodsMap.get(goodsname).getName() + "\t" +
+                    goodsMap.get(goodsname).getManufacturer() + "\t" +
+                    goodsMap.get(goodsname).getPrice() + "\t" +
+                    goodsMap.get(goodsname).getQuantity());
           writer.newLine();
-    
+        }
           System.out.println("Goods data written to file successfully.");
     
         } catch (IOException e) {
@@ -115,5 +212,43 @@ public class MyGoods {
           e.printStackTrace();
         }
       }
+    public void readFile(){
+        // 指定文件路径
+        String filePath = "Goods.txt";
+
+        try {
+            // 创建文件对象
+            File file = new File(filePath);
+            try (// 创建Scanner对象进行文件读取
+            Scanner scanner1 = new Scanner(file)) {
+                // 读取第一行，包含列名
+                String firstLine = scanner1.nextLine();
+                // 使用制表符分隔列名
+                String[] columnNames = firstLine.split("\t");
+
+                // 读取后续行的数据
+                while (scanner1.hasNextLine()) {
+                    String line = scanner1.nextLine();
+                    // 使用制表符分隔各列数据
+                    String[] data = line.split("\t");
+
+                    // 将数据存储到相应的变量中
+                    String id = data[0];
+                    String name = data[1];
+                    String manufacturer = data[2];
+                    float price = Float.parseFloat(data[3]);
+                    int quantity = Integer.parseInt(data[4]);
+                    MyGoods good=new MyGoods(id, name, manufacturer, price, quantity);
+                    goodsMap.put(good.getName(), good);
+                    
+                }
+            } catch (NumberFormatException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
