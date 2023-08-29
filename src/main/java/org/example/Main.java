4f43ca23471d065133648a661bd89c5fd2b22cee
Singawe Jack <chenming@mail.ynu.edu.cn>
2023-08-29 12:24:05 +0000

    121

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -16,25 +16,18 @@ public class Main {
         // init.createShopshistoryFile();
         
         List<MyAction> actionList = new ArrayList<MyAction>();
-
-
-
         MyAdminstrator ad=new MyAdminstrator(scanner);
         actionList.add(ad);
         Myuser user= new Myuser(scanner);
         actionList.add(user);
-
         String userInput = "";
-
         while (true) {
             System.out.println("请选择你的指令,ad管理员系统,user用户系统,q退出");
             System.out.print("你当前在第一级目录下 >");
             userInput = scanner.nextLine();
-
             if (userInput.equals("q")) {
                 break;
-            }
-            
+            } 
             String actionName = null;
             for(MyAction oneAction: actionList) {
                 actionName = oneAction.getActionName();
@@ -43,9 +36,7 @@ public class Main {
                     oneAction.run(null);
                 }
             }
-
         }
-
         scanner.close();
         System.out.println("Done.");
     }
